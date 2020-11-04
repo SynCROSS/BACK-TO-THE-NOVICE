3c59912a48fe6b7b6ebbbae4672cb459b5a960b5
SynCROSS <51585533+yuto3125@users.noreply.github.com>
2020-11-04 14:42:31 +0900

    Learning JS

---
+++ js/function.js
@@ -0,0 +1,7 @@
+function addAandB(a, b) {
+  return a + b;
+}
+const sayHi = function () {
+  console.log('Hello');
+};
+console.log(sayHi);
---
+++ js/if.js
@@ -0,0 +1,1 @@
+company == 'Netscape' ? alert('정답입니다!') : alert('오답입니다!'); // ! Don't Use code like that!.
