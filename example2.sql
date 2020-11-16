00505faef217e48c92cb1f8b28be771fc5748ec0
SynCROSS <51585533+yuto3125@users.noreply.github.com>
2020-11-16 10:13:18 +0900

    Update example2.sql

--- example2.sql
+++ example2.sql
@@ -67,8 +67,25 @@ WHERE
         product.id = payment_history.product_id
     AND product.price != payment_history.order_price / payment_history.order_count;
 -- * alpha problem 1.
+-- * 추가 문제 #1.
+-- * 카탈로그 테이블에 CATEGORY_ID 를 추가로 생성해주시고
+-- * NAME 기준으로 PRODUCT 테이블에 있는 데이터(카테고리 id)를 CATALOGUE 테이블에 추가해 주시고
+-- * CATALOGUE 테이블에서 FK(외래키) 를 설정해주세요
+-- * 서브 쿼리 사용 필요
 ALTER TABLE catalogue ADD category_id NUMBER;
-
+UPDATE catalogue
+SET
+    category_id = (
+        SELECT
+            product.category_id
+        FROM
+            product,
+            category
+        WHERE
+            product.category_id = category.id
+        ORDER BY
+            product.id
+    );
 ALTER TABLE catalogue
     ADD CONSTRAINT catalogue_fk FOREIGN KEY ( category_id )
         REFERENCES category ( id );
@@ -98,6 +115,16 @@ FROM
 WHERE
     product.id = payment_history.product_id;
 -- * alpha problem 2.
+SELECT
+    product.name    AS 상품명,
+    CASE
+        WHEN length(description) >= 10 THEN
+            concat(substr(description, 0, 10), '...')
+        ELSE
+            description
+    END             AS 상품설명
+FROM
+    product;
 -- * 9.
 SELECT
     product.name                   AS 상품명,
